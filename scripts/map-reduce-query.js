159c98adf36cd571d0d339fd0cb0d32b37b209c7
Kyo Suayan <ksuayan@gmail.com>
2013-01-31 22:52:45 -0800

    generate genres collection

--- scripts/map-reduce-query.js
+++ scripts/map-reduce-query.js
@@ -2,50 +2,24 @@ var mongodb = require('mongodb');
 
 // Connect to the db
 mongodb.connect("mongodb://127.0.0.1:27017/itunes", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-    
-    var collection = db.collection('trackdbs');
-    
-   // Map function
-   var map = function() { 
-       emit(this.Genre, {count:1}); 
-   };
-   
-   // Reduce function
-   var reduce = function(key, values) {
-        var sum = 0;
-        values.forEach(function(doc) {
-            sum += doc.count;
-        });
-        return {count:sum};
-    };
-    
-    /*
-    var stream = collection.find({Artist:"New Order"},{_id:0,Album:1,Name:1}).streamRecords();
-    stream.on("data", function(item) {
-        console.log("item",item);
-    });
-    stream.on("end", function() {
-        console.log("done.");
-    });
-    */
-   
-    collection.mapReduce(map, reduce, {out: {replace:"mapReduceResults"}});
-    
-    /*
-    var res = collection.mapReduce(map, reduce, {out : {inline : 1}}, function(err, results) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log(results[0].value.items);
-        }
-    });
-    */
-
-    
-    db.close();
-  } else {
-    console.log("error",err);
-  }
+    if(!err) {
+        console.log("We are connected");
+        var collection = db.collection('trackdbs');
+        var map = function() {
+            emit(this.Genre, 1);
+        };
+        // Reduce function
+        var reduce = function(previous, current) {
+            var count = 0;
+            for(index in current) {//in this example, 'current' will only have 1 index and the 'value' is 1
+                count += current[index];
+                //increments the counter by the 'value' of 1
+            }
+            return count;
+        };
+        collection.mapReduce(map, reduce, {out : {replace : "genres"}});
+        db.close();
+    } else {
+        console.log("error", err);
+    }
 });
