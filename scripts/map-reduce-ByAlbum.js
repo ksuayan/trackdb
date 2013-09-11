95368ea3ae05bd96ab0a10163cafd9dd00a2179c
wsuayan <wsuayan@stanfordmed.org>
2013-09-11 08:59:34 -0700

    map reduce scripts, updated mongoose verison

---
+++ scripts/map-reduce-ByAlbum.js
@@ -0,0 +1,11 @@
+db.trackdbs.mapReduce(
+    function() {
+        emit(this.Album,1);
+    },
+    function(k,values){
+        return Array.sum(values);
+    },
+    {
+        out: {replace:"ByAlbums"}
+    }
+);
